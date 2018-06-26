c0e0fc17ca7784f532358c8db1a748f7fd6f42ac
Lucas Bechberger <lbechberger@uos.de>
2018-06-26 08:58:17 +0200

    added regression line to plots for easier visual interpretation

--- InfoGAN-tf/plot_mapping.py
+++ InfoGAN-tf/plot_mapping.py
@@ -8,6 +8,7 @@ Created on Tue Jun 19 14:40:59 2018
 import sys
 import pickle
 import matplotlib.pyplot as plt
+from numpy.polynomial.polynomial import polyfit
 
 input_file = sys.argv[1]
 input_data = pickle.load(open(input_file, 'rb'))
@@ -26,12 +27,15 @@ for dim_idx, dimension in enumerate(dimension_names):
         ax = fig.add_subplot(n_latent, len(dimension_names), index)
         x = table[:,n_latent + dim_idx]
         y = table[:,latent_dim]
-        ax.scatter(x, y, marker='.')
+        ax.scatter(x, y, marker='.', s = 1)
         ax.set_xlabel(dimension)
         if dim_idx == 0:
             ax.set_ylabel("latent_{0}".format(latent_dim))
-        #if dimension == 'orientation_div':
-        #    ax.set_xscale('log')
+#        if dimension == 'orientation_div':
+#            ax.set_xscale('log')
+        b, m = polyfit(x, y, 1)
+        ax.plot(x, b + m * x, '-', color = "r", linewidth = 2)
+        
 
 plt.subplots_adjust(left=0.05, right=0.98, top=0.97, bottom=0.05, wspace = 0.3, hspace = 0.17)
 plt.show()
