8ea7193a6ff7e42a879e5734a44b65a8d26566dd
AnnaBruns <annabruns@uos.de>
2018-11-09 11:35:55 +0100

    copy of run_infogan.py without functions
    This script is a copy of run_infogan.py. The difference is that instead of
    defining the functions get_eval_noise, infogan_generator, float_image_to_uint8
    and infogan_discriminator, this script imports them from the script
    helperfunctions.py.

--- InfoGAN-tf/run_infogan_test.py
+++ InfoGAN-tf/run_infogan_test.py
@@ -7,9 +7,16 @@ Inspired by and based on
 and
     https://github.com/tensorflow/models/tree/master/research/gan/mnist
 
-Created on Thu Jan 25 2018
+This script is a copy of run_infogan.py. The difference is that instead of 
+defining the functions get_eval_noise, infogan_generator, float_image_to_uint8 
+and infogan_discriminator, this script imports them from the script 
+helperfunctions.py.
+
+Created on Thu Jan 25 2018,
 
 @author: lbechberger
+
+Edited on Fri Nov  9 by christina
 """
 
 import pickle
@@ -29,6 +36,8 @@ from six.moves import xrange
 from configparser import RawConfigParser
 
 from datetime import datetime
+
+from helperfunctions import get_eval_noise,infogan_generator,float_image_to_uint8,infogan_discriminator
 
 timestamp = str(datetime.now()).replace(' ','-')
 
@@ -90,48 +99,11 @@ print("Starting InfoGAN training. Here are my parameters:")
 print(options)
 print("Length of data set: {0}".format(length_of_data_set))
 
-# architecture of the generator network
-def infogan_generator(inputs):
-    with tf.contrib.framework.arg_scope(
-      [layers.fully_connected, layers.conv2d_transpose],
-      activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm,
-      weights_regularizer=layers.l2_regularizer(2.5e-5)):
-        unstructured_noise, cont_noise = inputs
-        noise = tf.concat([unstructured_noise, cont_noise], axis=1)
-        net = layers.fully_connected(noise, 1024)
-        net = layers.fully_connected(net, 7 * 7 * 128)
-        net = tf.reshape(net, [-1, 7, 7, 128])
-        net = layers.conv2d_transpose(net, 64, [4, 4], stride=2)
-        net = layers.conv2d_transpose(net, 32, [4, 4], stride=2)
-        # Make sure that generator output is in the same range as `inputs`, i.e. [-1, 1].
-        net = layers.conv2d(net, 1, [4, 4], normalizer_fn=None, activation_fn=tf.nn.tanh)
 
-        return net
 
 _leaky_relu = lambda x: tf.nn.leaky_relu(x, alpha=0.1)
 
-# architecture of the discriminator network
-def infogan_discriminator(img, unused_conditioning, weight_decay=2.5e-5, continuous_dim=2):
-    with tf.contrib.framework.arg_scope(
-    [layers.conv2d, layers.fully_connected],
-    activation_fn=_leaky_relu, normalizer_fn=None,
-    weights_regularizer=layers.l2_regularizer(2.5e-5),
-    biases_regularizer=layers.l2_regularizer(2.5e-5)):
-        net = layers.conv2d(img, 64, [4, 4], stride=2)
-        net = layers.conv2d(net, 128, [4, 4], stride=2)
-        net = layers.flatten(net)
-        net = layers.fully_connected(net, 1024, normalizer_fn=layers.layer_norm)
-    logits_real = layers.fully_connected(net, 1, activation_fn = None)
 
-    with tf.contrib.framework.arg_scope([layers.batch_norm], is_training=False):
-        encoder = layers.fully_connected(net, 128, normalizer_fn=layers.batch_norm, activation_fn=_leaky_relu)
-
-    # Compute mean for Gaussian posterior of continuous latents.
-    mu_cont = layers.fully_connected(encoder, continuous_dim, activation_fn=None)
-    sigma_cont = tf.ones_like(mu_cont)
-    q_cont = ds.Normal(loc=mu_cont, scale=sigma_cont)
-
-    return logits_real, [q_cont]
 
 
 def get_training_noise(batch_size, structured_continuous_dim, noise_dims):
@@ -156,52 +128,6 @@ def get_training_noise(batch_size, structured_continuous_dim, noise_dims):
         raise Exception("Unknown type of latent distribution: {0}".format(options['type_latent']))
 
     return [unstructured_noise], [continuous_noise]
-
-def get_eval_noise(noise_dims, continuous_sample_points, latent_dims, idx):
-    """Create noise showing impact of first dim continuous noise in InfoGAN.
-    First dimension of continuous noise is constant across columns. Other noise is
-    constant across rows.
-    Args:
-        noise_samples: Number of non-categorical noise samples to use.
-        continuous_sample_points: Possible continuous noise points to sample.
-        unstructured_noise_dims: Dimensions of the unstructured noise.
-        idx: Index of continuous dimension we want to evaluate.
-    Returns:
-        Unstructured noise, continuous noise numpy arrays."""
-
-    rows, cols = 20, len(continuous_sample_points)
-
-    # Take random draws for non-first-dim-continuous noise, making sure they are constant across columns.
-    unstructured_noise = []
-    for _ in xrange(rows):
-        cur_sample = np.random.normal(size=[1, noise_dims])
-        unstructured_noise.extend([cur_sample] * cols)
-    unstructured_noise = np.concatenate(unstructured_noise)
-
-    cont_noise_other_dim = []
-    for _ in xrange(rows):
-        cur_sample = np.random.choice(continuous_sample_points, size=[1, latent_dims-1])
-        cont_noise_other_dim.extend([cur_sample] * cols)
-    cont_noise_other_dim = np.concatenate(cont_noise_other_dim)
-
-    # Increase evaluated dimension of continuous noise from left to right, making sure they are constant across rows.
-    cont_noise_evaluated_dim = np.expand_dims(np.tile(continuous_sample_points, rows), 1)
-
-    if idx == 0:
-        # first dimension is the one to be evaluated
-        continuous_noise = np.concatenate((cont_noise_evaluated_dim, cont_noise_other_dim), 1)
-    elif idx == latent_dims - 1:
-        # last dimension is the one to be evaluated
-        continuous_noise = np.concatenate((cont_noise_other_dim, cont_noise_evaluated_dim), 1)
-    else:
-        # intermediate dimension is to be evaluated --> split other_dims_list in two parts
-        other_dims_list = np.split(cont_noise_other_dim, latent_dims - 1, axis=1)
-        first = np.concatenate(other_dims_list[:idx], 1)
-        second = np.concatenate(other_dims_list[idx:], 1)
-        # sneak cont_noise_evaluated_dim in the middle and glue everything back together
-        continuous_noise = np.concatenate((first, cont_noise_evaluated_dim, second), 1)
-
-    return unstructured_noise.astype('float32'), continuous_noise.astype('float32')
 
 
 # Build the generator and discriminator.
@@ -254,9 +180,6 @@ for epoch in options['epochs']:
 
 print("Number of training steps: {0}".format(max_num_steps))
 
-def float_image_to_uint8(image):
-    scaled = (image * 127.5) + 127.5
-    return tf.cast(scaled, tf.uint8)
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -282,6 +205,7 @@ with tf.Session(config=config) as sess:
                 os.makedirs(checkpoint_dir)
             saver = tf.train.Saver()
             saver.save(sess,os.path.join(checkpoint_dir, timestamp +'.model'))
+            print(saver)
             
             # create some output images for the current epoch
             CONT_SAMPLE_POINTS = np.linspace(-1.2, 1.2, 13)
