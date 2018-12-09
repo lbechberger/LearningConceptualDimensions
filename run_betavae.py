1cf90fc4eeaef2c029f06cf9522e5b5bb51d1353
Hermann <hermannsinger@gmx.net>
2018-12-09 15:23:07 +0100

    just committing to pull

---
+++ run_betavae.py
@@ -0,0 +1,174 @@
+"""
+This file uses slightly modified code from https://github.com/wiseodd/generative-models/blob/master/VAE/vanilla_vae/vae_tensorflow.py
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+import pickle
+import numpy as np
+import scipy.ndimage.filters as filters
+import argparse
+# Should be removed later on
+from tensorflow.examples.tutorials.mnist import input_data as inp_data
+
+# default values for options
+options = {}
+options['train_log_dir'] = 'logs'
+options['output_dir'] = 'output'
+options['training_file'] = '../data/uniform.pickle'
+options['noise_dims'] = 62
+options['latent_dims'] = 2
+options['batch_size'] = 128
+options['gen_lr'] = 1e-3
+options['dis_lr'] = 2e-4
+options['lambda'] = 1.0
+options['epochs'] = '50'
+options['type_latent'] = 'uniform'
+
+# Set up the input
+input_data = pickle.load(open(options['training_file'], 'rb'), encoding='latin1')
+rectangles = np.array(list(map(lambda x: x[0], input_data['data'])), dtype=np.float32)
+dimension_names = input_data['dimensions']
+length_of_data_set = len(rectangles)
+# images = rectangles.reshape((-1, 28, 28, 1))
+images = rectangles.reshape((-1, 784))
+
+# Uncomment if you want to use mnist instead of the rectangle data set
+""" 
+mnist = inp_data.read_data_sets('../../MNIST_data', one_hot=True)
+images = mnist.train.images
+"""
+
+mb_size = 64
+z_dim = 100
+X_dim = images.shape[1] 
+h_dim = 128
+c = 0
+lr = 1e-3
+beta = 1
+
+def get_batch(data):
+    '''
+    Return a total of `num` random samples and labels. Code taken from:
+    https://stackoverflow.com/questions/40994583/how-to-implement-tensorflows-next-batch-for-own-data 
+    '''
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:mb_size]
+    data_shuffle = [data[i] for i in idx]
+    
+    return data_shuffle
+
+
+def plot(samples):
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(4, 4)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+    return fig
+
+
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+
+# =============================== Q(z|X) ======================================
+
+X = tf.placeholder(tf.float32, shape=[None, X_dim])
+z = tf.placeholder(tf.float32, shape=[None, z_dim])
+
+Q_W1 = tf.Variable(xavier_init([X_dim, h_dim]))
+Q_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+Q_W2_mu = tf.Variable(xavier_init([h_dim, z_dim]))
+Q_b2_mu = tf.Variable(tf.zeros(shape=[z_dim]))
+
+Q_W2_sigma = tf.Variable(xavier_init([h_dim, z_dim]))
+Q_b2_sigma = tf.Variable(tf.zeros(shape=[z_dim]))
+
+
+def Q(X):
+    h = tf.nn.relu(tf.matmul(X, Q_W1) + Q_b1)
+    z_mu = tf.matmul(h, Q_W2_mu) + Q_b2_mu
+    z_logvar = tf.matmul(h, Q_W2_sigma) + Q_b2_sigma
+    return z_mu, z_logvar
+
+
+def sample_z(mu, log_var):
+    eps = tf.random_normal(shape=tf.shape(mu))
+    return mu + tf.exp(log_var / 2) * eps
+
+
+# =============================== P(X|z) ======================================
+
+P_W1 = tf.Variable(xavier_init([z_dim, h_dim]))
+P_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+P_W2 = tf.Variable(xavier_init([h_dim, X_dim]))
+P_b2 = tf.Variable(tf.zeros(shape=[X_dim]))
+
+
+def P(z):
+    h = tf.nn.relu(tf.matmul(z, P_W1) + P_b1)
+    logits = tf.matmul(h, P_W2) + P_b2
+    prob = tf.nn.sigmoid(logits)
+    return prob, logits
+
+
+# =============================== TRAINING ====================================
+
+z_mu, z_logvar = Q(X)
+z_sample = sample_z(z_mu, z_logvar)
+_, logits = P(z_sample)
+
+# Sampling from random z
+X_samples, _ = P(z)
+
+# E[log P(X|z)]
+recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
+# D_KL(Q(z|X) || P(z)); calculate in closed form as both dist. are Gaussian
+kl_loss = beta * 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
+# VAE loss
+vae_loss = tf.reduce_mean(recon_loss + kl_loss)
+
+solver = tf.train.AdamOptimizer().minimize(vae_loss)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+if not os.path.exists('out/'):
+    os.makedirs('out/')
+
+i = 0
+
+for it in range(1000000):
+    # X_mb, _ = mnist.train.next_batch(mb_size)
+    X_mb = get_batch(images)
+    #print(X_mb)
+
+    _, loss = sess.run([solver, vae_loss], feed_dict={X: X_mb})
+
+    if it % 1000 == 0:
+        print('Iter: {}'.format(it))
+        print('Loss: {:.4}'. format(loss))
+        print()
+
+        samples = sess.run(X_samples, feed_dict={z: np.random.randn(16, z_dim)})
+
+        fig = plot(samples)
+        plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        i += 1
+plt.close(fig)
