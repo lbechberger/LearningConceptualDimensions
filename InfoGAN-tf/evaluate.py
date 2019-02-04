4be7141777044b27088682da46756759c3cf2948
Hermann <hermannsinger@gmx.net>
2019-02-04 20:05:38 +0100

    Define default to_add

--- InfoGAN-tf/evaluate.py
+++ InfoGAN-tf/evaluate.py
@@ -4,8 +4,14 @@ import csv
 
 # TO-DO: Replace value later on
 config_name = 'foo'
-# TO-DO: Replace value later on
-cats = ['f', 'o', 'o']
+
+DEF = '-'
+# Initialize to_add with DEFault value
+to_add = {}
+# TO-DO: Replace with correct categories later on
+for cat in ['f', 'o']:
+    to_add[cat] = DEF
+
 """
 csv_name = 'eggs.csv'
 # Fail-fast if csv file does not exist or first line unequal to cats
@@ -23,11 +29,18 @@ def reg_mse_and_score(inputs, labels):
     mseReg = sk.metrics.mean_squared_error(labels, pred)
     return mseReg, reg.score(inputs, pred)
 
-def add_to_csv(cats, csv_name, to_add):
+def add_to_csv(ordered_cats, csv_name, to_add):
+    """
+
+    :param ordered_cats: category names in the correct order
+    :param csv_name: csv file's name
+    :param to_add: dict that maps category names to their values
+    :return: void
+    """
     with open(csv_name, 'a', newline='') as csvfile:
         # If value for category missing, fill in with '-'
         keys = to_add.keys()
-        for cat in cats:
+        for cat in ordered_cats:
             if not cat in keys:
                 to_add[cat] = '-'
-        csv.writer(csvfile).writerow([to_add[cat] for cat in cats])
+        csv.writer(csvfile).writerow([to_add[cat] for cat in ordered_cats])
