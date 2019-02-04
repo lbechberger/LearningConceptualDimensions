8164b185e8bcda7ee39eba3d26ff142f429e2dc3
Hermann <hermannsinger@gmx.net>
2019-02-04 18:31:52 +0100

    Removed some comments

--- InfoGAN-tf/evaluate.py
+++ InfoGAN-tf/evaluate.py
@@ -1,18 +1,6 @@
-"""
-CSV
-
-"""
 import numpy as np
 import sklearn as sk
 import csv
-
-
-
-"""
-# TO-DO: Insert appropriate arguments into fit 
-
-dann einfach die euklidische Distanz berechnen und r^2 kriegt man eh mit reg.score
-"""
 
 def reg_mse_and_score(inputs, labels):
     from sklearn.linear_model import LinearRegression
