caff5d6b01871733a9e591a8a778eac6859a2743
Hermann <hermannsinger@gmx.net>
2019-02-12 16:49:19 +0100

    Too general: update_dict

--- InfoGAN-tf/evaluate.py
+++ InfoGAN-tf/evaluate.py
@@ -7,8 +7,8 @@ def load_np_pickle(ind):
     import sys
     pickle.load(open(sys.argv[ind], 'rb'), encoding='latin1')
 
-input_data = load_np_pickle(0)
-evaluation_data = load_np_pickle(1)
+#input_data = load_np_pickle(0)
+#evaluation_data = load_np_pickle(1)
 
 
 # TO-DO: Replace value later on
@@ -17,19 +17,17 @@ config_name = 'foo'
 DEF = '-'
 # Initialize to_add with DEFault value
 to_add = {}
-# TO-DO: Replace with correct categories later on
-ordered_cats = ['f', 'o']
-for cat in ordered_cats:
+# TO-DO: Add the remaining categories later on
+ORDERED_CATS = ['mseReg', 'r^2']
+for cat in ORDERED_CATS:
     to_add[cat] = DEF
 
 # TO-DO: Replace with correct csv-name later on
-CSV_NAME = 'eggs.csv'
+CSV_NAME = 'bla.csv'
 
-# Fail-fast if csv file does not exist or first line unequal to to_add.keys()
+# Fail-fast if csv file does not exist or first line unequal to the categories
 with open(CSV_NAME, 'r', newline='') as csvfile:
-    if not (
-            ordered_cats
-            == next(csv.reader(csvfile))):
+    if not (ORDERED_CATS == next(csv.reader(csvfile))):
         raise ValueError("CSVFile's first line unequal config/metric-names")    
 
 
@@ -40,7 +38,7 @@ def reg_mse_and_score(inputs, labels):
     :param labels
     :return: mse and r^2 coefficient between the labels and predicted values based on the inputs
     """
-    if not len(inputs) == labels(inputs):
+    if not len(inputs) == len(labels):
         raise ValueError
 
     from sklearn.linear_model import LinearRegression
@@ -51,7 +49,7 @@ def reg_mse_and_score(inputs, labels):
 
 def add_to_csv(ordered_cats, csv_name, to_add):
     """
-    adds to_add to csv with csvname. If values for a category are missing, '-' will be added
+    adds to_add to csv with csv_name. If values for a category are missing, '-' will be added
 
     :param ordered_cats: category names in the correct order
     :param csv_name: csv file's name
@@ -59,9 +57,27 @@ def add_to_csv(ordered_cats, csv_name, to_add):
     :return: void
     """
     with open(csv_name, 'a', newline='') as csvfile:
-        # If value for category missing, fill in with '-'
         keys = to_add.keys()
         for cat in ordered_cats:
+            # If value for a cat(egory) is missing, fill in with '-'
             if not cat in keys:
                 to_add[cat] = '-'
         csv.writer(csvfile).writerow([to_add[cat] for cat in ordered_cats])
+
+def update_dict(cats, dict, key, val):
+    """
+    Only allows val to be added to dict[key] if key is one of the categories in cats.
+
+    :param cats: the categories
+    :param dict:
+    :param key:
+    :param val:
+    :return:
+    """
+    if(key not in cats):
+        raise ValueError("IllegalKey")
+    dict[key] = val
+
+# wrapper function for update_dict, since update_dict's arguments "ordered_cats" and "dict"
+# stay constant in this script
+update_to_add = lambda key, val: update_dict(ORDERED_CATS, to_add, key, val)
